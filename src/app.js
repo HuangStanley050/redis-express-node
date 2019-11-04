43b9b9ca9f2b961e41b7eee044877cdeecb6d6a6
Stanley Huang <huang.stanley050@gmail.com>
2019-11-04 19:50:19 +1100

    connected

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const redis = require("redis");
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({ host: "redis-server" });
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
   client.get("visits", (err, visits) => {
     res.send(`Number of visits ${visits}`);
-    client.set("visists", parseInt(visits) + 1);
+    client.set("visits", parseInt(visits) + 1);
+    console.log(`${visits}`);
   });
 });
 
